cc6f411493d3f737d69ca6bae37e56ef23ca2874
Nivdek <>
2023-11-01 22:19:09 +0000

    Map out some early functions to help structrue

--- run.py
+++ run.py
@@ -1,6 +1,19 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
-test = "Hello"
+import random
 
-print(test)
+# Global variables
+grid_size = 10
+
+
+print("Welcome to Battleships!")
+print("Your objective is to the sink the enemy fleet!\n")
+
+def create_grid():
+    """
+    Function to create a 10x10 grid with randomly placed ships
+    """
+    pass
+
+
+
+
+
