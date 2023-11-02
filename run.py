76b7c4e52e0243f42b5e4c6581f0c1a70cf34703
Nivdek <>
2023-11-02 00:47:33 +0000

    Created functions to place ships upon the grid

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ GRID_SIZE = 10
 SIZE_OF_SHIPS = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1]
 NUM_OF_SHIPS = 10
 SHIP_POSITIONS = [[]]
+ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 # NUM_OF_SHIPS_SUNK (Use this if you can make program track ships else use NUM_OF_HITS to win (20 hits to win))
 
 
@@ -43,23 +44,93 @@ def create_grid():
         random_row = random.randint(0, rows - 1)
         random_col = random.randint(0, cols -1 )
         direction = random.choice(["up", "down", "left", "right"])
-        ship_size = SIZE_OF_SHIPS[0]
-        if try_to_place_ship_on_grid(random_row, random_col, direction, ship_size):
+        ship_size = SIZE_OF_SHIPS[0] #Not sure how to iterate through this to pull the correct ship sizes
+        if try_to_place_ships(random_row, random_col, direction, ship_size):
             num_of_ships_placed += 1
             ship_size += 1 # Unsure if this is the correct way to loop through ship sizes.
-    pass
 
 
-def place_ships():
+
+def place_ships(start_row, end_row, start_col, end_col):
     """
-    Function to place ships on respective boards (player/cpu)
-    - Should not print location as cpu board needs to be hidden
-    - Import ships from list to get correct sizes?
-    - Make sure ship placemenet cant overlap
-    - 10 ships (1, 1, 1, 1, 2, 2, 2, 3, 3, 4)
+    Checks the row and column values to make sure they are not previously occupied
     """
-    pass
+    global GRID
+    global SHIP_POSITIONS
 
+    all_valid = True
+    for r in range(start_row, end_row):
+        for c in range(start_col, end_col):
+            if GRID[r][c] != "~":
+                all_valid = False
+                break
+    if all_valid:
+        SHIP_POSITIONS.append([start_row, end_row, start_col, end_col])
+        for r in range(start_row, end_row):
+            for c in range(start_col, end_col):
+                GRID[r][c] = "O"
+    return all_valid
+
+
+def try_to_place_ships(row, col, direction, length):
+    """
+    Function will try designate a location within the grid for a ship using a helper method to make sure the location is not already occupied
+    """
+    global GRID_SIZE
+
+    start_row, end_row, start_col, end_col = row, row + 1, col, col + 1
+    if direction == "up":
+        if row - length < 0:
+            return False
+        start_row = row - length + 1
+    elif direction == "down":
+        if row + length >= GRID_SIZE:
+            return False
+        end_row = row + length
+    elif direction == "left":
+        if col - length < 0:
+            return False
+        start_col = col - length + 1
+    elif direction == "right":
+        if col + length >= GRID_SIZE:
+            return False
+        end_col = col + length
+    return place_ships(start_row, end_row, start_col, end_col)
+
+
+def print_grid():
+    """Will print the grid with rows A-J and columns 0-9"""
+    global GRID
+    global ALPHABET
+
+    debug_mode = True
+
+    ALPHABET = ALPHABET[0: len(GRID) + 1]
+
+    for row in range(len(GRID)):
+        print(ALPHABET[row], end=") ")
+        for col in range(len(GRID[row])):
+            if GRID[row][col] == "O":
+                if debug_mode:
+                    print("O", end=" ")
+                else:
+                    print(".", end=" ")
+            else:
+                print(GRID[row][col], end=" ")
+        print("")
+
+    print("  ", end=" ")
+    for i in range(len(GRID[0])):
+        print(str(i), end=" ")
+    print("")
+
+
+
+def main():
+    create_grid()
+    print_grid()
+
+main()
 
 def print_cpu_board():
     """
@@ -82,5 +153,6 @@ def score_system():
     This function should check if you hit a ship and both update the board visually and increase your internal score (when score/hits reach 20 you win OR track number of ships and when ships sunk = 10 game is won/lost)
     """
     pass
+
 
 
