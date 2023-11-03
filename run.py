3cb0cf74e8ad836f045fddc5569caefe1801a1c3
Nivdek <>
2023-11-03 23:51:58 +0000

    defined methods for the main shooting function of the game

--- run.py
+++ run.py
@@ -8,21 +8,24 @@ SIZE_OF_SHIPS = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1]
 NUM_OF_SHIPS = 10
 SHIP_POSITIONS = [[]]
 ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+NUM_OF_SHIPS_SUNK = 0
 
 
 def place_ships(start_row, end_row, start_col, end_col):
     """
-    Checks the row and column values to make sure they are not previously occupied
+    Checks the row and column values to make sure they are not already occupied
     """
     global GRID
     global SHIP_POSITIONS
 
     all_valid = True
+    # for loop makes sure all the cells where a ship is trying to be placed is equal to Water
     for row in range(start_row, end_row):
         for column in range(start_col, end_col):
             if GRID[row][column] != "~":
                 all_valid = False
                 break
+    # if condition updates the value of the grids cells to O if a ship was succesfully placed there
     if all_valid:
         SHIP_POSITIONS.append([start_row, end_row, start_col, end_col])
         for row in range(start_row, end_row):
@@ -33,7 +36,8 @@ def place_ships(start_row, end_row, start_col, end_col):
 
 def try_to_place_ships(row, col, direction, length):
     """
-    Function will try designate a location within the grid for a ship using a helper method to make sure the location is not already occupied
+    Function will try designate a location within the grid for a ship using a helper method to make sure the location is not already occupied.
+    It makes sure all ships have their starting and ending positional values stay within the confines of the defined grid size.
     """
     global GRID_SIZE
 
@@ -72,9 +76,8 @@ def create_grid():
     global SIZE_OF_SHIPS
 
     random.seed()
-
+    # Creates a grid and assigns the games symbol for Water as a value to them
     rows, cols = (GRID_SIZE, GRID_SIZE)
-
     GRID = []
     for row in range(rows):
         row = []
@@ -83,12 +86,14 @@ def create_grid():
         GRID.append(row)
 
     num_of_ships_placed = 0
-
+    # While loop to place as many ships as are stated in the NUM_OF_SHIPS variable. meant to be defined at game start.
     while num_of_ships_placed != NUM_OF_SHIPS:
         random_row = random.randint(0, rows - 1)
         random_col = random.randint(0, cols - 1)
         direction = random.choice(["left", "right", "up", "down"])
+        # This uses the SIZE_OF_SHIPS list to determine size of ships, this is meant to be determined at the start of the game.
         ship_size = SIZE_OF_SHIPS[num_of_ships_placed]
+        # Uses try_to_place_ships to make sure ships are placed inside of the grid and do not overlap.
         if try_to_place_ships(random_row, random_col, direction, ship_size):
             num_of_ships_placed += 1
 
@@ -103,11 +108,12 @@ def print_grid():
     debug_mode = True
 
     ALPHABET = ALPHABET[0: len(GRID) + 1]
-
+    # for loop to apped a letter infront of every row of the grid.
     for row in range(len(GRID)):
         print(ALPHABET[row], end=") ")
         for col in range(len(GRID[row])):
             if GRID[row][col] == "O":
+                # this debug feature lets you just debug_mode to true in order to see the positions of ships printed on the board.
                 if debug_mode:
                     print("O", end=" ")
                 else:
@@ -120,6 +126,25 @@ def print_grid():
     for i in range(len(GRID[0])):
         print(str(i), end=" ")
     print("")
+
+
+def shot_placement():
+    """
+    Contains input for where the player wants to shoot and also handles any faulty input attempts.
+    """
+    
+
+
+def sunk_ships():
+    """
+    Contains a check to see if a ship has been sunk.
+    """
+
+
+def print_shot():
+    """
+    Prints the placement of your shot on the board.
+    """
 
 
 def main():
